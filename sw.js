894d73977de0a62566325af6267cfa66acf7515a
TUANBANG <tanvd.baobituanbang@gmail.com>
2026-02-25 18:59:16 +0700

    feat: Cache background image and prefetch assets
    Adds the Heidelberg background image to the service worker cache and prefetches it in `index.html` to improve initial load performance.
    Also refactors the fetch event in `sw.js` to apply a cache-first strategy for images and fonts, further enhancing offline capabilities and loading speed.

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  'https://i.postimg.cc/8zF3c24h/image.png'
+  'https://i.postimg.cc/8zF3c24h/image.png',
+  'https://i.postimg.cc/Ls5LPN1Z/Heidelberg-1.jpg'
 ];
 
 self.addEventListener('install', (event) => {
@@ -17,10 +18,32 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  const requestUrl = new URL(event.request.url);
+
+  // Cache strategy for images and fonts
+  if (
+    requestUrl.hostname === 'i.postimg.cc' ||
+    requestUrl.hostname === 'fonts.googleapis.com' ||
+    requestUrl.hostname === 'fonts.gstatic.com'
+  ) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          const fetchPromise = fetch(event.request).then((networkResponse) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+          return response || fetchPromise;
+        });
+      })
+    );
+    return;
+  }
+
+  // Default strategy for other requests
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
